31a0c784eb990535195db37b442f02a42b8e63e4
matthidm <57016504+matthidm@users.noreply.github.com>
2022-06-28 23:57:44 +0200

    not predefined var error removed

--- main.py
+++ main.py
@@ -4,26 +4,16 @@ import math
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-def error_message(msg):
-    print(msg)
-    input("Click ENTER to exit")
-    exit()
-
 def parse_line(line:str, params:dict) -> str:
-    if "ER" in line:
-        if "ER" not in params:
-            error_message("ERROR: Undefined parameter used: ER")
-        else:
-            x = re.findall("X-?(\d+\.?\d*)", line)
-            y = re.findall("Y-?(\d+\.?\d*)", line)
-            x = float(x[0]) if len(x) == 1 else 0
-            y = float(y[0]) if len(y) == 1 else 0
-            l = math.sqrt(x**2+y**2)
-            return line.replace("ER", f"E {params['ER']*l:.2f}")
+    if "ER" in params and "ER" in line:
+        x = re.findall("X-?(\d+\.?\d*)", line)
+        y = re.findall("Y-?(\d+\.?\d*)", line)
+        x = float(x[0]) if len(x) == 1 else 0
+        y = float(y[0]) if len(y) == 1 else 0
+        l = math.sqrt(x**2+y**2)
+        return line.replace("ER", f"E {params['ER']*l:.2f}")
 
-    elif "ZH" in line:
-        if "ZH" not in params:
-            error_message("ERROR: Undefined parameter used: ZH")
+    elif "ZH" in params and "ZH" in line:
         return line.replace("ZH", f"Z {params['ZH']:.2f}")
     else:
         return line
@@ -32,13 +22,14 @@ def search_parameters(line:str, params:dict):
     if "VAR" in line:
         tokens = line.split(" ")
         if len(tokens) != 3:
-            error_message("ERROR: variable undefined")
+            print("ERROR: variable undefined")
+            input("Click ENTER to exit")
+            exit()
         params[tokens[1]] = float(tokens[2])
 
     
 def main():
-    # Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    input_file = askopenfilename(initialdir = os.getcwd(), filetypes=[("Gcode","*.gcode"), ("Text","*.txt")])
+    input_file = askopenfilename(initialdir = os.getcwd(), filetypes=[("Gcode","*.gcode"), ("Text","*.txt"), ("Other files", "*")])
     output_file = input_file.replace(".","_output.")
     params = dict()
 
